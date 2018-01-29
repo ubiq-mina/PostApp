a2db0f4ae1df36ef18458bd451d567d0d10e1294
Gian Mina <gsmina@ubiquitous-tech.com>
2018-01-29 15:45:25 +0800

    Implemented form model binding in profile edit.

--- resources/views/editprofile.blade.php
+++ resources/views/editprofile.blade.php
@@ -19,25 +19,43 @@
                     </p>
 
                     <div class="card mb-3">
-                        <div class="card-body">
-                            <p class="card-text"></p>
-                            {!! $info = ""; !!}
-                            
-                            {!! Form::model($info, array('url' => '/profile')) !!}
 
-                            {{--  <row>  --}}
-                                {{--  <div class='col-md-11 form-group'>  --}}
-                                    {!! Form::label('username', 'Username'); !!}
-                                    {!! Form::text('username', '', ['class' => 'form-control']); !!}
-                                {{--  </div>  --}}
-                                {{--  <div class='col-md-1'>  --}}
-                                    {!! Form::submit('Send', ['class' => 'btn btn-default']); !!}
-                                {{--  </div>  --}}
-                            {{--  </row>  --}}
+                            <div class="card-header">
+                                Personal Information
+                            </div>
+    
+                            <div class="card-body">
+                                <p class="card-text">
 
-                            {!! Form::close() !!}
+                                    {!! Auth::user()->profile !!}
+                    
+                                    {!! Form::model(Auth::user()->profile, array('url' => '/intro', 'id' => "personal-info")) !!}
+        
+                                    {!! Form::label('birthdate', 'Birthdate'); !!}
+                                    {!! Form::date('birthdate', null, ['class' => 'form-control mb-5']); !!}
+        
+                                    {!! Form::label('country', 'Country'); !!}
+                                    {!! Form::select('country', [null => 'Select a country', 'Philippines' => 'Philippines'], null, ['class' => 'form-control mb-5']); !!}
+        
+                                    {!! Form::label('mobile', 'Mobile number') !!}
+                                    {!! Form::text('mobile', null, ['class' => 'form-control mb-5']) !!}
+        
+                                    {!! Form::label('sex', 'Gender') !!} <br>
+        
+                                    {!! Form::label('gender-male', 'Male', ['class' => 'ml-3']); !!}
+                                    {!! Form::radio('sex', 'Male', false, ['id' => 'gender-male']); !!}
+        
+                                    {!! Form::label('gender-female', 'Female'); !!}
+                                    {!! Form::radio('sex', 'Female', false, ['id' => 'gender-female', 'class' => 'mb-5']); !!} <br>
+        
+                                    {{--  {!! Form::submit('Next', ['id' => 'personal-info', 'class' => 'btn btn-default']); !!}  --}}
+                                    <input type="submit" name="personal-info" class="btn btn-default" value="Save">
+        
+                                    {!! Form::close() !!}
+    
+                                </p>
+                            </div>
                         </div>
-                    </div>
 
                 </div>
             </div>
